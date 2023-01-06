f6b19e3bad31bbffea136888a477efabc51c9920
Daktic <g@eye-catcher.com>
2023-01-05 20:42:02 -0500

    Refactored getEnvelopeBycategory and added in explicit failure on what category was not returned. Refactored redundent newBudget variable.

--- server-side/spend.js
+++ server-side/spend.js
@@ -2,21 +2,16 @@ const express = require("express");
 const spendRouter = express.Router();
 
 // db imports
-const {
-    envelopes,
-    getEnvelopeBycategory
-} = require("./db");
-
-// PUT for subtracting the input amount from specific envelop's budget
+const {getEnvelopeBycategory} = require("./db");
+//PUT for subtracting the input amount from specific envelop's budget
 spendRouter.put("/:categoryName", (req, res, next) =>{
     const envelopeParam = req.params.categoryName;
     const envelopeBody = req.body;
 
     const envelopeToUpdate = getEnvelopeBycategory(envelopeParam);
-    const newBudget = envelopeToUpdate[0].budget - envelopeBody.budget;
-    envelopeToUpdate[0].budget = newBudget
+    envelopeToUpdate[0].budget -= envelopeBody.budget;
 
-    res.send(`You remaing budget: $${envelopeToUpdate[0].budget}`);
+    res.send(`You remaining budget: $${envelopeToUpdate[0].budget}`);
 });
 
 module.exports = spendRouter;
