bd259655be609a68f85773abfed15a347a33629a
Saim Ali Nadeem <saimalinadeem4@gmail.com>
2023-01-28 07:11:25 +0500

    db_integration

--- server/route/spending-category.js
+++ server/route/spending-category.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const categoryRouter = express.Router();
+const pool = require("../db")
 
 // utils import
 const {
@@ -9,49 +10,83 @@ const {
 } = require("./utils");
 
 // => API/spending-category routes 
-categoryRouter.get("/", (req, res) =>{
-    try{
-      res.send(envelopes);
-    }catch(err) {
-      next(err)
+categoryRouter.get("/", (req, res, next) =>{
+    try {
+       pool.query('SELECT * FROM spending_category', (err, result)=>{
+        if(err){
+          throw err
+        }
+        res.send(result.rows);
+       });
+    } catch (err) {
+        next(err)
     }
 });
 
 categoryRouter.get("/:categoryName", (req, res, next) =>{
-    const envelopeParam = req.params.categoryName;
+    const categoryName = req.params.categoryName;
     try{
-      res.send(getEnvelopeBycategory(envelopeParam));
+      const selectQuery = `SELECT * FROM spending_category 
+      WHERE spending_category.category = '${categoryName}'`
+      pool.query(selectQuery, (err, result) =>{
+        if(err){
+          throw err;
+        };
+        res.send(result.rows);
+      });
     }catch(err) {
-      next(err)
-    }
+      next(err);
+    };
 });
 
 categoryRouter.post("/", (req, res, next) =>{
-    const envelopeBody = req.body;
+  const {id, category, budget} = req.body;
     try{
-      envelopes.push(envelopeBody);
-      res.status(201).send("Envelope successfully added");
-    }catch(err) {
+       const insertQuery = `INSERT INTO spending_category (id, category, budget)
+       VALUES (${id}, '${category}', ${budget})`
+
+       pool.query(insertQuery, (err, result) =>{
+        if(err){
+          throw err
+        }
+        res.status(201).send("Envelope successfully added");
+       })
+    } catch(err) {
       next(err)
     }
 });
 
-categoryRouter.put("/:categoryName", updateEnvelopeByIndex, (req, res, next) =>{
+categoryRouter.put("/:categoryName", (req, res, next) =>{
+  const categoryName = req.params.categoryName;
+  const {id, category, budget} = req.body;
     try{
-      res.status(200).send("Envelope successfully updated");
+      const updateQuery = `UPDATE spending_category
+      SET id = ${id}, category = '${category}', budget = ${budget}
+      WHERE category = '${categoryName}'`
+
+      pool.query(updateQuery, (err, result) =>{
+        if(err){
+          throw err;
+        }
+        res.send(`Category: ${categoryName} has been updated`)
+      });
     }catch(err) {
       next(err)
     }
 });
 
 categoryRouter.delete("/:categoryName", (req, res, next)=>{
-    const envelopeParam = req.params.categoryName;
+    const categoryName = req.params.categoryName;
     try{
-      // Find the index & remove the enevelop
-      const envelopeIndex = envelopes.findIndex(env => env.category === envelopeParam);
-      envelopes.splice(envelopeIndex, 1);
+      const deleteQuery = `DELETE FROM spending_category
+      WHERE category = '${categoryName}'`
 
-      res.status(204).send("Envelope successfully deleted");
+      pool.query(deleteQuery, (err, result) =>{
+        if(err){
+          throw err;
+        }
+        res.status(204).send("Envelope successfully deleted");
+      })
     }catch(err) {
       next(err)
    }
